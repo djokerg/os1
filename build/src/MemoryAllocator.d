49628d8b7204bf1f0402cc90d36b201873293225
DjordjeG <117691152+djokerg@users.noreply.github.com>
2023-05-05 15:12:33 +0200

    Delete build directory

--- build/src/MemoryAllocator.d
+++
@@ -1,6 +0,0 @@
-build/src/MemoryAllocator.o: src/MemoryAllocator.cpp \
- src/../h/MemoryAllocator.hpp src/../h/../lib/hw.h
-
-src/../h/MemoryAllocator.hpp:
-
-src/../h/../lib/hw.h:
--- build/test/userMain.d
+++
@@ -1,1 +0,0 @@
-build/test/userMain.o: test/userMain.cpp
